cf30e491de205196f8dc7d4557eb293b831f2c7f
Ibrahim AlRayyan <ibrahim.alrayany@gmail.com>
2022-03-24 04:59:17 +0200

    add questions page

---
+++ src/pages/questions.jsx
@@ -0,0 +1,38 @@
+import React, { useMemo } from 'react';
+import { useParams, Link } from 'react-router-dom';
+import { Container, Row, Col } from 'reactstrap';
+import QuestionCard from '../components/question-card';
+
+const Questions = ({ quizzes }) => {
+  const quizId = useParams().id;
+  const questions = useMemo(() => {
+    return quizzes.find(q => q.id === quizId)?.questions_answers;
+  }, [quizzes, quizId]);
+
+  return (
+    <section>
+      <Container>
+        <Row className="mb-3">
+          <Col>
+            <h4>Questions</h4>
+          </Col>
+          <Col md="auto">
+            <Link to="/">
+              <button type="button" className="btn btn-link">Back</button>
+            </Link>
+          </Col>
+        </Row>
+        <Row className="justify-content-center">
+          {questions.map((quest) => (
+            <Col lg={9} key={quest.id}>
+              <QuestionCard data={quest} />
+            </Col>
+          ))
+          }
+        </Row>
+      </Container>
+    </section >
+  );
+};
+
+export default Questions;
