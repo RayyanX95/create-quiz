b974cce7d90d092594e4e1dbb70f4a0e24f9f505
Ibrahim AlRayyan <ibrahim.alrayany@gmail.com>
2022-03-24 05:00:19 +0200

    add logic to question card component

--- src/components/question-card/index.jsx
+++ src/components/question-card/index.jsx
@@ -1,13 +1,42 @@
-import React from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import { FormGroup, Input, Label, Row } from 'reactstrap';
 import { Col } from 'reactstrap';
 
-const QuestionCard = () => {
+const QuestionCard = ({ data }) => {
+  const [answers, setAnswers] = useState([]);
+
+  const addShowFeedbackPropToData = useCallback(() => {
+    const tempAnswers = data.answers.map((ans) => ({ ...ans, showFeedback: false }));
+    setAnswers(tempAnswers);
+  }, [data.answers]);
+
+  useEffect(() => {
+    addShowFeedbackPropToData();
+  }, [addShowFeedbackPropToData]);
+
+  const resetAllRadiosToFalse = (tempAnswers) => {
+    return tempAnswers.map((ans) => ({
+      ...ans,
+      showFeedback: false,
+    }));
+  }
+
+  const selectAnswerHandler = (id) => {
+    const ansIndex = answers.findIndex(ans => ans.id === id);
+    const tempAnswers = resetAllRadiosToFalse(answers, ansIndex);
+    tempAnswers[ansIndex] = {
+      ...tempAnswers[ansIndex],
+      showFeedback: true,
+    }
+
+    setAnswers(tempAnswers);
+  }
+
   return (
-    <section className="border question-card w-75 mx-auto">
+    <section className="question-card">
       <Row>
         <Col md={12}>
-          <h6 className="mb-1">Q. question 1 text</h6>
+          <h6 className="mb-1">{data.text ? data.text : 'Q. question 1 text'}</h6>
         </Col>
       </Row>
       <Row className="justify-content-center mb-1">
@@ -16,54 +45,24 @@ const QuestionCard = () => {
         </Col>
       </Row>
       <Row>
-        <Col md={6}>
-          <FormGroup check>
-            <Input
-              name="radio1"
-              type="radio"
-            />
-            {' '}
-            <Label check className="small">
-              Option two can be something else and selecting it will deselect option one
-            </Label>
-          </FormGroup>
-        </Col>
-        <Col md={6}>
-          <FormGroup check>
-            <Input
-              name="radio1"
-              type="radio"
-            />
-            {' '}
-            <Label check className="small">
-            Option Option Option Option Option Option Option Option two can be something else and selecting it will deselect option one
-            </Label>
-          </FormGroup>
-        </Col>
-        <Col md={6}>
-          <FormGroup check>
-            <Input
-              name="radio1"
-              type="radio"
-            />
-            {' '}
-            <Label check className="small">
-              Option two can be something else and selecting it will deselect option one
-            </Label>
-          </FormGroup>
-        </Col>
-        <Col md={6}>
-          <FormGroup check>
-            <Input
-              name="radio1"
-              type="radio"
-            />
-            {' '}
-            <Label check className="small">
-              Option two can be something else and selecting it will deselect option one
-            </Label>
-          </FormGroup>
-        </Col>
+        {answers.map((ans) => (
+          <Col md={6} key={ans.id} className="mb-2">
+            <FormGroup check>
+              <Input
+                name={data.id}
+                type="radio"
+                onChange={() => selectAnswerHandler(ans.id)}
+              />
+              {' '}
+              <Label check className="small mb-0">
+                {ans.text ? data.text : 'Option two can be something else and selecting it will deselect option one'}
+              </Label>
+              <p className={`feedback ${ans.is_true ? 'text-success' : 'text-danger'} ${!ans.showFeedback && 'hidden'}`}>
+                {ans.is_true ? (data.feedback_true || 'Correct answer') : (data.feedback_false || 'Wrong answer')}
+              </p>
+            </FormGroup>
+          </Col>
+        ))}
       </Row>
     </section>
   );
